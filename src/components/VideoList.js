68918fdd0727e20639cacdde9966895e879f024c
kanjana <82321119+Kkanjana@users.noreply.github.com>
2023-09-11 20:26:02 +0700

    fix dis play video list

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,13 +2,14 @@ import React from "react";
 import videoData from "../video-data.json";
 import { Link } from "react-router-dom";
 
+
 const VideoList = () => {
-    const onMouseOverVideo = (e) => {
-        e.target.play();
-    }
-    const onMouseOutVideo = (e) => {
-        e.target.pause();
-    }
+    // const onMouseOverVideo = (e) => {
+    //     e.target.play();
+    // }
+    // const onMouseOutVideo = (e) => {
+    //     e.target.pause();
+    // }
 
     return (
         <div className="video-list">
@@ -18,15 +19,16 @@ const VideoList = () => {
             <div className="row">
                 {videoData.map((item) => (
                     <div className="col-md-3" key={item.id}>
-                        <div className="card">
+                        <div className="card h-100">
                             <Link to={`/video/${item.id}`}>
-                                <video className="card-video" 
-                                    muted loop 
-                                    onMouseOver={onMouseOverVideo} 
-                                    onMouseOut={onMouseOutVideo}
+                                {/* <video className="card-video" 
+                                    // muted loop 
+                                    // onMouseOver={onMouseOverVideo} 
+                                    // onMouseOut={onMouseOutVideo}
                                 >
                                     <source src={`/videos/${item.url}`} type="video/mp4"/>
-                                </video>
+                                </video> */}
+                                <img src={`/videos/${item.img}`} alt={`${item.img}`} className="card-img-top"></img>
                             </Link>
                             <div className="card-body">
                                 <p className="card-text-header">{item.title}</p>
