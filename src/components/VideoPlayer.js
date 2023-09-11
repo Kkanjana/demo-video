32a1ff2c4a833a8502cc0111c15f897e558f865a
kanjana <82321119+Kkanjana@users.noreply.github.com>
2023-09-11 22:17:07 +0700

    update css

--- src/components/VideoPlayer.js
+++ src/components/VideoPlayer.js
@@ -8,19 +8,21 @@ const VideoPlayer = () => {
 
     return (
         <div className="video-player">
-            <div className="position-absolute top-50 start-50 translate-middle">
-                <div className="card card-video-player">
-                    <video className="video-player-body" controls autoPlay={true}>
-                        <source src={`${video.url}`} type="video/mp4" />
-                    </video>
-                    <div className="card-body">
-                        <p className="card-text-header">{video.title}</p>
+            <div className="row">
+                <div className="col-12">
+                    <div className="card card-video-header">
+                        <video className="card-img-top" controls autoPlay={true}>
+                            <source src={`${video.url}`} type="video/mp4" />
+                        </video>
+                        <div className="card-body">
+                            <p className="card-text-header">{video.title}</p>
+                        </div>
                     </div>
-                </div>
-                <div className="col-12 back-page"> 
-                    <Link to="/">
-                        <button type="button" className="btn btn-danger">BACK</button>
-                    </Link>
+                    <div className="col-12 back-page"> 
+                        <Link to="/">
+                            <button type="button" className="btn btn-danger">BACK</button>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </div>
